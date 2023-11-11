6a20029ddf8ab7554cc0539f392dc29d513b74bd
Jeremy C <jchuq27@gmail.com>
2023-11-11 13:58:59 -0800

    commit

---
+++ conditionals.js
@@ -0,0 +1,12 @@
+prompt("What year were you born?")
+birthYear = prompt
+console.log("The value of birthYear is, prompt");
+if ((2023 - birthYear) < 21){
+    alert("Sorry, you are not old enough to drink in the US");
+}
+if ((2023 - birthyear) > 21){
+    alert("You are old enough to drink in the US")
+}
+if((2023 - birthYear) === 21){
+    alert("You are old enough to drink in the US...barely")
+}
