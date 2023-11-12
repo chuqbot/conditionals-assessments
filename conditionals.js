9717fd4a3918dbd309b5b0e94f5df47da20dd05b
Jeremy C <jchuq27@gmail.com>
2023-11-12 12:56:54 -0800

    commit

--- conditionals.js
+++ conditionals.js
@@ -1,5 +1,24 @@
+var userChoice = 0
+var randomNumber = math.randomNumber
+var computerChoice = 1
+
+prompt("Heads or Tails?")
+
+
+
+
+
+
+
+
+
+
+
+
+birthYear = prompt("What year were you born?")
+
 prompt("What year were you born?")
-birthYear = prompt
+
 console.log("The value of birthYear is, prompt");
 if ((2023 - birthYear) < 21){
     alert("Sorry, you are not old enough to drink in the US");
