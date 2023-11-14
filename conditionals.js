716d072de715870ecbd5538adac493c4b372320e
Jeremy C <jchuq27@gmail.com>
2023-11-13 17:10:40 -0800

    commit

--- conditionals.js
+++ conditionals.js
@@ -1,3 +1,32 @@
+var userChoice;
+var randomNumber;
+var computerChoice;
+
+userChoice = prompt("Heads or tails?");
+randomNumber = Math.random();
+
+if (randomNumber < 0.5) {
+    randomNumber = 0
+}
+else {
+    randomNumber + 1
+}
+
+if (randomNumber == 0) {
+    computerChoice = "heads";
+} else {
+    computerChoice = "tails";
+}
+
+if (userChoice == computerChoice) {
+    alert("You guessed right! The coin flip landed on" + userChoice);
+} else {
+    if (computerChoice == "heads") {
+        alert("Sorry, the coin flip landed on heads");
+    } else if (computerChoice == "tails"){
+        alert("sorry, the coin flip landed on tails");
+    }
+}
 
 const birthYear=prompt("What year were you born?")
 
