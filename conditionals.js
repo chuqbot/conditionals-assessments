6ff7efdd8fdfe0065e7cb6e4481a52b8ef0a2422
Jeremy C <jchuq27@gmail.com>
2023-11-13 17:06:47 -0800

    lol

--- conditionals.js
+++ conditionals.js
@@ -1,29 +1,12 @@
 
-prompt("Heads or Tails?")
+const birthYear=prompt("What year were you born?")
 
-
-
-
-
-
-
-
-
-
-
-
-
-prompt("What year were you born?")
-
-birthYear = prompt("What year were you born?")
-
-console.log("The value of birthYear is, prompt");
 if ((2023 - birthYear) < 21){
     alert("Sorry, you are not old enough to drink in the US");
 }
-if ((2023 - birthyear) > 21){
+else if ((2023 - birthYear) > 21){
     alert("You are old enough to drink in the US")
 }
-if((2023 - birthYear) === 21){
+else if((2023 - birthYear) === 21){
     alert("You are old enough to drink in the US...barely")
 }
